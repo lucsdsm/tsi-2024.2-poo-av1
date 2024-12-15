5b0dad2fbcf0be5d907170c491e1e2c7dcee8814
Lucas Eduardo <lucaseduardo168@gmail.com>
2024-12-15 06:16:31 -0300

    adicionadas classes jogo, carta, tabuleiro e biblioteca

--- boardgames/src/app/body/body.component.ts
+++ boardgames/src/app/body/body.component.ts
@@ -3,6 +3,8 @@ import { Jogo } from '../../models/jogo';
 import { Tabuleiro } from '../../models/tabuleiro';
 import { Carta } from '../../models/carta';
 import { Biblioteca } from '../../models/biblioteca';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-body',
@@ -12,5 +14,47 @@ import { Biblioteca } from '../../models/biblioteca';
   styleUrl: './body.component.css'
 })
 export class BodyComponent {
-  
+  biblioteca = new Biblioteca();
+
+  titulo = '';
+  anoPublicacao = 0;
+  fabricante = '';
+  numeroJogadores?: number;
+  idadeMinima?: number;
+  tipo = 'tabuleiro'; // Valor padrão
+
+  constructor() {
+    // Adicionando jogos para teste
+    this.biblioteca.adicionarJogo(new Tabuleiro('Catan', 1995, 'Kosmos', 4));
+    this.biblioteca.adicionarJogo(new Carta('UNO', 1971, 'Mattel', 7));
+  }
+
+  get jogos(): Jogo[] {
+    return this.biblioteca.listarJogos();
+  }
+
+  adicionarJogo(): void {
+    if (this.tipo === 'tabuleiro' && this.numeroJogadores) {
+      this.biblioteca.adicionarJogo(
+        new Tabuleiro(this.titulo, this.anoPublicacao, this.fabricante, this.numeroJogadores)
+      );
+    } else if (this.tipo === 'carta' && this.idadeMinima) {
+      this.biblioteca.adicionarJogo(
+        new Carta(this.titulo, this.anoPublicacao, this.fabricante, this.idadeMinima)
+      );
+    }
+    this.limparFormulario();
+  }
+
+  removerJogo(titulo: string): void {
+    this.biblioteca.removerJogo(titulo);
+  }
+
+  limparFormulario(): void {
+    this.titulo = '';
+    this.anoPublicacao = 0;
+    this.fabricante = '';
+    this.numeroJogadores = undefined;
+    this.idadeMinima = undefined;
+  }
 }
